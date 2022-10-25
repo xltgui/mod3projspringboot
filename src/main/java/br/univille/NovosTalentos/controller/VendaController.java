eb5d515d74d9316c9f7f13b1320876764dc7de70
xltgui <borba.liver@gmail.com>
2022-10-25 17:28:06 -0300

    WIP: Cadastro de vendas

--- src/main/java/br/univille/NovosTalentos/controller/VendaController.java
+++ src/main/java/br/univille/NovosTalentos/controller/VendaController.java
@@ -2,7 +2,6 @@ package br.univille.NovosTalentos.controller;
 
 import java.util.HashMap;
 
-import org.springframework.aop.framework.adapter.DefaultAdvisorAdapterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.univille.NovosTalentos.entity.Cliente;
+
 import br.univille.NovosTalentos.entity.Venda;
 import br.univille.NovosTalentos.service.ClienteService;
 import br.univille.NovosTalentos.service.ProdutoService;
