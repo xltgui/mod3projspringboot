3fc47c58a4f369775ffbc0515ff4b79482f17076
xltgui <borba.liver@gmail.com>
2022-11-01 15:24:09 -0300

    cybersecurity

--- src/main/java/br/univille/NovosTalentos/api/ClienteControllerAPI.java
+++ src/main/java/br/univille/NovosTalentos/api/ClienteControllerAPI.java
@@ -7,25 +7,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.univille.NovosTalentos.entity.Cliente;
 import br.univille.NovosTalentos.service.ClienteService;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("api/v1/clientes")
 public class ClienteControllerAPI {
     @Autowired
     private ClienteService service;
-
+    
+    @ApiOperation(value = "Endpoint que retorna todos os dados dos clientes")
     @GetMapping
     public ResponseEntity<List<Cliente>> getAll() {
         var listaClientes = service.getAll();
--- src/test/java/br/univille/NovosTalentos/NovosTalentosApplicationTests.java
+++ src/test/java/br/univille/NovosTalentos/NovosTalentosApplicationTests.java
@@ -1,13 +1,62 @@
 package br.univille.NovosTalentos;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import br.univille.NovosTalentos.api.ClienteControllerAPI;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class NovosTalentosApplicationTests {
+@AutoConfigureMockMvc
+class NovostalentosApplicationTests {
+
+	@Autowired
+	private ClienteControllerAPI clienteControllerAPI;
+	
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
+		assertThat(clienteControllerAPI).isNotNull();
+
+	}
+
+	@Test
+	void verificaCliente() throws Exception{
+		mockMvc.perform(get("/api/v1/clientes"))
+		.andExpect(status().isOk());
+	}
+
+	@Test
+	void verificaClienteRetorno() throws Exception{
+		MvcResult result =
+        mockMvc.perform(get("/api/v1/clientes"))
+		.andExpect(status().isOk()).andReturn();
+		assertTrue(result.getResponse().getContentAsString().contains("gui"));
+	}
+
+	@Test
+	void verificaClientePeloNome() throws Exception{
+		MvcResult result = 
+		mockMvc.perform(get("/api/v1/clientes/nome/ze"))
+		.andExpect(status().isOk())
+		.andReturn();
+	assertTrue(result.getResponse().getContentAsString().contains("gui"));
 	}
 
 }
